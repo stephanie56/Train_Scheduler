34c0e94369cc44474fdec7bf79a596a28e1ad8a4
lmarchet <leo.marchetti@hotmail.ca>
2018-05-23 15:21:22 -0400

    update_1

---
+++ javascript.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+//FIREBASE=========================================================
+
+// Initialize Firebase - Provided by firebase webpage
+var config = {
+    apiKey: "AIzaSyC3N4z4WbhhXyxD3LHvI4Ksxm_VUi_3yrY",
+    authDomain: "train-scheduler-21669.firebaseapp.com",
+    databaseURL: "https://train-scheduler-21669.firebaseio.com",
+    projectId: "train-scheduler-21669",
+    storageBucket: "",
+    messagingSenderId: "1094266374433"
+  };
+  firebase.initializeApp(config);
+
+
+var database = firebase.database();
+
+// create variables to hold schedule information: train name, destionation, departure/arrival time and frequency of travel
+
+// need to transfer train schedule data to firebase	
+
+// reference to new train data to the firebase database
+
+// calculate when the next train will arrive; this should be relative to the current time.
+	
+ //append the train data in the webpage
+
+
+}); // end of DOM
